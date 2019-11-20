f59663099e92adf09bfb04c279f4192b48b6ac16
senthil <senthiln0281@gmail.com>
2019-11-21 00:28:03 +0530

    terrpath

---
+++ app.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/WillKoehrsen/recurrent-neural-networks.git
+cd recurrent-neural-networks
+pip3 install --user -r requirements.txt
+cd deployment
+python3 run_keras_server.py
